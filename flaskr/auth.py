aa78311d71a74b321457fb7dc8cdf0adac7f9ecf
yaudev <yaudev2601@gmail.com>
2020-12-21 14:31:02 -0500

    Finished auth.py

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -6,7 +6,19 @@ from flask import (
 from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
+#creating blueprint
 bp = Blueprint('auth',__name__,url_prefix='/auth')
+
+#load in user id
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else: 
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
 
 #writing view code
 @bp.route('/register', methods=('GET','POST'))
@@ -35,3 +47,41 @@ def register():
             return redirect(url_for('auth.login'))
         flash (error)
     return render_template('auth/register.htmml')
+
+#login 
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+        if user is None:
+            error = 'Incorrect Username.'
+        elif not check_password_hash(user['password'],password):
+           error = 'Incorrect Password.'    
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+        flash(error)
+    return render_template('auth/login.html')
+
+#logout
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+#login required actions
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+    return wrapped_view
